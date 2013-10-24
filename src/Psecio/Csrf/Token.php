5bca91c89441ba921e039cb2eb45ee76cb888943
enygma <enygma@phpdeveloper.org>
2013-10-24 12:43:27 -0500

    adding docblock to Token::generate

--- src/Psecio/Csrf/Token.php
+++ src/Psecio/Csrf/Token.php
@@ -4,6 +4,14 @@ namespace Psecio\Csrf;
 
 class Token
 {
+	/**
+	 * Given a type and possible options, generate the token
+	 * 
+	 * @param string $type Method for generating the token
+	 * @param array $options Additional options for the request
+	 * @throws \InvalidArgumentException If type is invalid/not supported
+	 * @return mixed Either false on failure or the hash on success
+	 */
 	public function generate($type, array $options = array())
 	{
 		$tokenClass = "\\Psecio\\Csrf\\Token\\".ucwords(strtolower($type));
